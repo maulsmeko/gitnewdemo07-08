f3f77f82cbf3aeaf3504dfb325577ad6a8b85f16
maulik <mshah7329@gmail.com>
2020-08-07 23:54:27 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+
+	@Test
+	public void jiraAPI()
+	{
+		System.out.println("jiraAPI");
+	}
+	
+	@Test
+	public void jiraAPI2()
+	{
+		System.out.println("jiraAPI2");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void browserautomation()
+	{
+		System.out.println("browserautomation");
+	}
+	
+	@Test
+	public void elementsui()
+	{
+		System.out.println("elementsui");
+	}
+	
+}
